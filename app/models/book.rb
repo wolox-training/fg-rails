dea33589115296122a0b3367c3de58f5e1a21b5d
franguerini <frantguerini@gmail.com>
2019-09-25 16:00:17 -0300

    fix rubocop

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class Book < ApplicationRecord
-    validates :genre, presence: true
-    validates :author, presence: true
-    validates :image, presence: true
-    validates :title, presence: true
-    validates :editor, presence: true
-    validates :year, presence: true
+  validates :genre, presence: true
+  validates :author, presence: true
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :editor, presence: true
+  validates :year, presence: true
 end
