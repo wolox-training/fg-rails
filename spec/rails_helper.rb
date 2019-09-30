dbc7b7a2b7f003486fdd6fb1eceec618c40d745a
franguerini <frantguerini@gmail.com>
2019-09-30 18:14:05 -0300

    added pathç

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __dir__)
+require File.expand_path('../../config/environment', __PATH__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
@@ -12,7 +12,7 @@ require 'support/factory_bot'
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
 # in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
+# run twice. It is recommended that you do not name files matching xsthis glob to
 # end with _spec.rb. You can configure this pattern with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
 #
